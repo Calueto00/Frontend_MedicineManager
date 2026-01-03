e4e40dba6ce06e9b7da71ba98a4032e40517d0cd
00Calueto <paulo.calueto.francisco@gmail.com>
2026-01-03 10:47:34 +0100

    doctor details adjustmnts

--- src/pages/DoctorDetails.jsx
+++ src/pages/DoctorDetails.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import api from "../api/axios";
 import { useParams } from "react-router-dom";
 import AppointmentList from "../components/dashboard/doctors/AppointmentList";
@@ -7,17 +7,30 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const day = {
+    1: "segunda",
+    2: "Terça",
+    3: "Quarta",
+    4: "Quinta",
+    5: "Sexta",
+    6: "Sábado",
+    7: "Domingo",
+};
+
 export default function DoctorDetails() {
     const { id } = useParams();
     const [doctor, setDoctor] = useState([]);
-    const [appointments, setAppointments] = useState([])
+    const [appointments, setAppointments] = useState([]);
+    const [schedules, setSchedules] = useState([]);
+    const [loading, setLoading] = useState(true);
     const fetchData = async () => {
         try {
             await api.get(`/doctor/${id}`).then((response) => {
-                setDoctor(response.data);
-
-                setAppointments(response.data.appointments);
+                setDoctor(response.data.doctor);
+                setSchedules(response.data?.schedules);
+                setAppointments(response.data?.doctor.appointments);
             });
+            setLoading(false);
         } catch (error) {
             console.error(error);
         }
@@ -65,7 +78,9 @@ export default function DoctorDetails() {
                     <AppointmentList appointments={appointments} />
 
                 </div>
-                <div className="w-80">
+
+                {/**doctor statistics */}
+                <div className="w-100 space-y-2">
                     <div className="bg-white rounded-lg p-2 shadow-md space-y-2">
                         <div className="flex items-center justify-between">
                             <h3 className="text-sm font-semibold">Doctor Statistics</h3>
@@ -76,17 +91,17 @@ export default function DoctorDetails() {
                                     data={{
                                         labels: [
                                             `Total Appointments (${appointments.length})`,
-                                            `Confirmed (${appointments.filter(a => a.status === 'confirmado').length})`,
                                             `Scheduled (${appointments.filter(a => a.status === 'agendado').length})`,
-                                            `Completed (${appointments.filter(a => a.status === 'completado').length})`
+                                            `Completed (${appointments.filter(a => a.status === 'concluido').length})`,
+                                            `Canceled (${appointments.filter(a => a.status === 'cancelado').length})`
                                         ],
                                         datasets: [
                                             {
                                                 label: 'Appointment Status',
                                                 data: [
                                                     appointments.length,
-                                                    appointments.filter(a => a.status === 'concluido').length,
                                                     appointments.filter(a => a.status === 'agendado').length,
+                                                    appointments.filter(a => a.status === 'concluido').length,
                                                     appointments.filter(a => a.status === 'cancelado').length
                                                 ],
                                                 backgroundColor: [
@@ -110,10 +125,10 @@ export default function DoctorDetails() {
                                         maintainAspectRatio: true,
                                         plugins: {
                                             legend: {
-                                                position: 'bottom',
+                                                position: 'top',
                                                 labels: {
-                                                    font: { size: 11 },
-                                                    padding: 10
+                                                    font: { size: 13 },
+                                                    padding: 8
                                                 }
                                             }
                                         }
@@ -123,6 +138,48 @@ export default function DoctorDetails() {
                         ) : (
                             <p className="text-sm text-gray-500 text-center py-8">Nenhum agendamento encontrado</p>
                         )}
+                    </div>
+                        {/**schedules list */}
+                    <div className="bg-white rounded-lg p-2 space-y-2 shadow-sm border border-slate-300">
+                        <div className="text-sm flex items-center justify-between bg-blue-700 rounded-md p-1 text-white">
+                            <h3 className="font-semibold">Doctor Schedules</h3>
+                            <span>Total ({schedules?.length || 0})</span>
+                        </div>
+                        <div className="space-y-2">
+                                {
+                                    loading && 
+                                    <div className="flex items-center justify-center p-2">
+                                        <div 
+                                        className="animate-spin border-r-3 w-8 h-8 
+                                        text-center rounded-full">
+                                        </div>
+                                    </div>
+                                }
+
+                                {
+                                    schedules?.length === 0 ? (
+                                    <div className="p-2 text-sm text-gray-400 text-center">
+                                        No schedules found</div>) : ( schedules?.map(schedule => (
+                                            <div key={schedule.id} 
+                                            className="flex items-center bg-slate-100 shadow-sm justify-between rounded-lg border-slate-300 border p-1">
+                                                <lable>
+                                                    <h3 className="text-xs text-gray-600">Day</h3>
+                                                    <span className="text-sm">{day[schedule.day_weeks]}</span>
+                                                </lable>
+                                                <lable>
+                                                    <h3 className="text-xs text-gray-600">Start at</h3>
+                                                    <span className="text-sm">{schedule.start_time}</span>
+                                                </lable>
+                                                <lable>
+                                                    <h3 className="text-xs text-gray-600">End at</h3>
+                                                    <span className="text-sm">{schedule.end_time}</span>
+                                                </lable>
+                                            </div>
+                                        ))
+
+                                    )
+                                }
+                        </div>
                     </div>
                 </div>
             </section>
