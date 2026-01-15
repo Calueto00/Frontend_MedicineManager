4e805cd46a37ba170d79aa2b64b5a5bc99167536
00Calueto <paulo.calueto.francisco@gmail.com>
2026-01-15 08:58:06 +0100

    adjustment appointment list

--- src/components/dashboard/doctors/AppointmentList.jsx
+++ src/components/dashboard/doctors/AppointmentList.jsx
@@ -1,15 +1,15 @@
-
-
+import { Link } from "react-router-dom"
 export default function AppointmentList({appointments}){
+    
     return (
         <section className="bg-white p-2 rounded-lg space-y-3 shadow-md">
             <div className="flex items-center bg-blue-800 rounded text-white justify-between border-b border-slate-300 p-2">
                 <span className="text-sm font-semibold">Appointment List</span>
-                <span className="text-sm">Total - {appointments.length}</span>
+                <span className="text-sm">Total - {appointments?.length}</span>
             </div>
             
                     {
-                        appointments.length > 0 ? (
+                        appointments?.length > 0 ? (
                             <table className="min-w-full">
                                 <thead className="text-sm text-gray-600">
                                     <tr>
@@ -21,12 +21,33 @@ export default function AppointmentList({appointments}){
                                 </thead>
                                 <tbody>
                                     {
-                                        appointments.map((appointment)=>(
-                                            <tr key={appointment.id} className="text-center border-t-4 border-white bg-slate-200">
-                                                <td className="p-1">{appointment?.patient?.user.name}</td>
-                                                <td>{appointment.date_houra}</td>
-                                                <td>{appointment.status}</td>
-                                                <td>detail</td>
+                                        appointments.map((appointments)=>(
+                                            <tr key={appointments.id} className="text-center border-t-4 border-white bg-slate-200">
+                                                <td className="p-1">{appointments?.patient?.user.name}</td>
+                                                <td>{appointments.data}</td>
+                                                <td>
+                                                    {appointments.status === 'pending' 
+                                                    && <span 
+                                                    className="bg-orange-600 p-1 rounded text-white font-semibold">
+                                                        {appointments.status}</span>}
+                                                    {appointments.status === 'denied' 
+                                                    && <span 
+                                                    className="bg-red-600 p-1 rounded text-white font-semibold">
+                                                        canceled</span>}
+                                                    
+                                                    {appointments.status === 'confirmed' 
+                                                    && <span 
+                                                    className="bg-blue-600 p-1 rounded text-white font-semibold">
+                                                        {appointments.status}</span>}
+                                                    
+                                                    {appointments.status === 'concluded' 
+                                                    && <span 
+                                                    className="bg-green-500 p-1 rounded text-white font-semibold">
+                                                        {appointments.status}</span>}
+                                                </td>
+                                                <td>
+                                                    <Link className="font-semibold text-white bg-blue-800 text-sm px-2 py-1 rounded" to={`/dashboard/appointment/${appointments.id}`}>See</Link>
+                                                </td>
                                             </tr>
                                         ))
                                     }
